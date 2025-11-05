c7af29a52da3457d8efc2c985841753b7b7ea25d
Victor-Hck <victor.profissional3592@gmail.com>
2025-11-05 19:00:22 -0300

    refactor(layout,page,footer,header):reorganize all component/indentation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,20 @@ import Image from "next/image";
 
 const Home = () => {
     return(
-        <div className="flex justify-center rounded-lg  my-3 w-300 bg-[#080c1e] transition-all duration-200 hover:-translate-1">
-            <div className="p-8 font-bold mt-10">
+        <div className="flex flex-col lg:flex-row justify-center rounded-lg my-5
+            w-full max-w-6xl bg-[#080c1e] transition-all duration-200 hover:-translate-y-1">
+
+            <div className="p-8 font-bold mt-10 max-w-xl">
                 <p className="text-2xl text-blue-500">Hello World 👋</p>
-                <h1 className="text-5xl text-white animate-bounce  [text-shadow:2px_2px_10px_blue]">Eu sou o Victor</h1>
-                <h2 className="text-4xl text-[#616060] animate-pulse [text-shadow:0_0_10px_green]">Desenvolvedor Web Front-end</h2>
+
+                <h1 className="text-5xl text-white animate-bounce [text-shadow:2px_2px_10px_blue]">
+                    Eu sou o Victor
+                </h1>
+
+                <h2 className="text-3xl text-[#616060] animate-pulse [text-shadow:0_0_10px_green]">
+                    Desenvolvedor Web Front-end
+                </h2>
+
                 <button
                     onClick={() => {
                         const link = document.createElement("a");
@@ -16,36 +25,46 @@ const Home = () => {
                         link.click();
                     }}
                     className="border rounded-lg mt-4 px-3 py-3 cursor-pointer
-                    font-bold text-white bg-purple-600 transition-all duration-200 hover:-translate-1
-                animate-pulse shadow-[0_0_15px_#a855f7]">Baixar CV</button>
-                <p className="mt-7">
+                    font-bold text-white bg-purple-600 transition-all duration-200 hover:-translate-y-1
+                    animate-pulse shadow-[0_0_15px_#a855f7]">
+                    Baixar CV
+                </button>
+
+                <p className="mt-5 text-sm leading-relaxed">
                     Com 24 anos, sou um profissional entusiasta por tecnologia e com uma paixão genuína por tudo que a envolve.
                     Possuo uma forte inclinação para o estudo contínuo e dedico-me ativamente à atualização constante dos meus conhecimentos e habilidades. 
                     Acredito que a tecnologia avança rapidamente, e por isso, mantenho um ritmo de aprendizado ininterrupto para garantir que minhas competências estejam sempre alinhadas com as exigências e inovações do mercado de trabalho.
                     Minha dedicação em estudar continuamente não é apenas uma obrigação, mas uma motivação intrínseca para me qualificar e contribuir de forma significativa para os desafios e projetos das empresas. 
                     Estou sempre em busca de novas ferramentas, metodologias e tendências para aplicar na prática e agregar valor.
                 </p>
-                <div className="border-l-3 mt-10">
-                    <p className="pl-2 text-sm [text-shadow:1px_1px_11px_white]">Ser desenvolvedor é uma viagem onde a próxima parada é a solução de um problema.</p>
+
+                <div className="border-l-4 border-white mt-10">
+                    <p className="pl-2 text-sm [text-shadow:1px_1px_11px_white]">
+                        Ser desenvolvedor é uma viagem onde a próxima parada é a solução de um problema.
+                    </p>
                 </div>
-                <p className="mt-2 indent-107 text-sm hover:underline cursor-pointer">Autor: Thales Valentim</p>
+
+                <p className="mt-2 text-sm hover:underline cursor-pointer">
+                    Autor: Thales Valentim
+                </p>
             </div>
-            <div className="relative flex w-1000 group cursor-pointer">
+
+            <div className="flex justify-center relative w-full max-w-[500px] h-[400px] group cursor-pointer mt-40">
                 <Image
                     src={"/nika-sungod.gif"}
                     alt="Luffy Gear five - One Piece"
-                    quality={100}
-                    fill
-                    className="transition-opacity duration-300 opacity-100 group-hover:opacity-0 border-white"
+                    width={420}
+                    height={420}
+                    className="object-contain transition-opacity duration-300 opacity-100 group-hover:opacity-0"
                 />
                 <Image
                     src={"/luffy-gear5.gif"}
                     alt="Luffy Gear five - One Piece"
-                    quality={75}
                     fill
-                    className="transition-opacity duration-300 opacity-0 hover:opacity-100 absolute top-0 left-0"
+                    className="object-contain transition-opacity duration-300 opacity-0 group-hover:opacity-100 absolute top-0 left-0"
                 />
             </div>
+
         </div>
     );
 };
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -4,36 +4,54 @@ import { useAudio } from "@/hooks/PlayStop";
 
 export const Header = () => {
     const { play, pause, isPlaying } = useAudio("/Drums-of-Liberation.mp3");
-    return(                                                                                 // border-black shadow-[0_0_10px_#ff00e6]  animate-pulse
-        <header className="flex sticky top-0 z-50 justify-between items-center h-16 px-10 
-            bg-deepSpace/80 backdrop-blur-md border-b border-neonPink/60 shadow-[0_0_10px_#ff00e6] animate-pulse">
-            <div>
-                <h1 className="text-4xl font-bold text-shadow-[0_0_10px_#a855f7] animate-pulse transition-all duration-200 hover:-translate-1 cursor-pointer">Victor Gomes</h1>  
-            </div>
-            <nav className="">
+
+    return (
+        <header className="
+            sticky top-0 z-50
+            flex justify-between items-center
+            h-16 px-4 md:px-10
+            bg-deepSpace/80 backdrop-blur-md
+            border-b border-neonPink/60
+            shadow-[0_0_10px_#ff00e6] animate-pulse
+        ">
+            <h1 className="text-2xl md:text-4xl font-bold [text-shadow:0_0_10px_#a855f7] animate-pulse hover:-translate-y-1 duration-200 cursor-pointer">
+                Victor Gomes
+            </h1>
+
+            <nav className="hidden md:flex">
                 <div className="flex gap-3">
                     <a 
-                        className="border text- font-bold text-white bg-purple-600 animate-pulse transition-all duration-200 hover:-translate-1 
-                        text-1xl shadow-[0_0_10px_#a855f7] rounded-lg p-1"
-                        href="./">Tecnologias</a>
+                        className="border font-bold text-white bg-purple-600 animate-pulse duration-200 hover:-translate-y-1
+                        text-lg shadow-[0_0_10px_#a855f7] rounded-lg px-3 py-1 cursor-pointer"
+                    >Tecnologias</a>
                     
-                    <a 
-                        className="border font-bold text-white bg-purple-600 animate-pulse transition-all duration-200 hover:-translate-1
-                        text-1xl shadow-[0_0_10px_#a855f7] rounded-lg p-1"
-                        href="./">Projetos</a>
+                    <a className="border font-bold text-white bg-purple-600 animate-pulse duration-200 hover:-translate-y-1
+                        text-lg shadow-[0_0_10px_#a855f7] rounded-lg px-3 py-1 cursor-pointer"
+                    >Projetos</a>
                 </div>
             </nav>
-            <div className="flex gap-3">
-                <a href="https://github.com/Victor-Hck" target="_blank" className="border rounded-lg border-white text-white font-bold animate-pulse transition-all duration-200 hover:-translate-1
-                bg-cyberYellow shadow[2px_2px_10px_#0a0f24] p-1 cursor-pointer">GitHub</a>
-                <a href="https://www.linkedin.com/in/victor-antônio-11596b218/" target="_blank" className="border rounded-lg border-white text-white font-bold animate-pulse transition-all duration-200 hover:-translate-1
-                bg-cyberYellow shadow[2px_2px_10px_#0a0f24] p-1 cursor-pointer">Linkdim</a>
+
+            <div className="flex gap-2 md:gap-3">
+                <a 
+                    href="https://github.com/Victor-Hck"
+                    target="_blank"
+                    className="border rounded-lg border-white text-white font-bold animate-pulse duration-200 hover:-translate-y-1
+                    bg-cyberYellow px-3 py-1"
+                >GitHub</a>
+
+                <a 
+                    href="https://www.linkedin.com/in/victor-antônio-11596b218/"
+                    target="_blank"
+                    className="border rounded-lg border-white text-white font-bold animate-pulse duration-200 hover:-translate-y-1
+                    bg-cyberYellow px-3 py-1"
+                >LinkedIn</a>
+
                 <button
                     onClick={isPlaying ? pause : play}
-                    className="border rounded-lg border-white text-white font-bold animate-pulse transition-all duration-200 hover:-translate-1
-                bg-cyberYellow shadow[2px_2px_10px_#0a0f24] p-1 cursor-pointer"
+                    className="border rounded-lg border-white text-white font-bold animate-pulse duration-200 hover:-translate-y-1
+                    bg-cyberYellow px-3 py-1 cursor-pointer"
                 >
-                    {isPlaying ? "Pausar música" : "Tocar música"}
+                    {isPlaying ? "Pausar" : "Tocar música"}
                 </button>
             </div>
         </header>
